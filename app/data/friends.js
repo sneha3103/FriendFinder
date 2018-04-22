03c17ae8c461ae9a76db329be6ff0423dcd0d2e5
Sneha Dama <snehadama@Snehas-Air.lan>
2018-04-22 12:32:56 -0500

    Updated a survey question and completed the friends javascript file

---
+++ app/data/friends.js
@@ -0,0 +1,139 @@
+[
+    {
+        "name":"Rachel Green",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            4,
+            3,
+            1,
+            2,
+            5,
+            5,
+            3,
+            5,
+            1,
+            4
+          ]
+    },
+
+    {
+        "name":"Chandler Bing",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,
+            2,
+            2,
+            2,
+            4,
+            3,
+            2,
+            5,
+            4,
+            3
+          ]
+    },
+
+    {
+        "name":"Joey Tribbiani",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            1,
+            4,
+            5,
+            4,
+            2,
+            5,
+            2,
+            2
+          ]
+    },
+
+    {
+        "name":"Ross Geller",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            2,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            2,
+            2,
+            4
+          ]
+    },
+
+    {
+        "name":"Phoebe Buffet",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            2,
+            2,
+            4,
+            5,
+            2,
+            5,
+            3,
+            2
+          ]
+    },
+
+    {
+        "name":"Monica Geller",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            3,
+            3,
+            5,
+            5,
+            3,
+            1,
+            2,
+            1,
+            5,
+            4
+          ]
+    },
+
+
+    {
+        "name":"Janice",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            4,
+            1,
+            2,
+            3,
+            5,
+            3,
+            2,
+            2,
+            4,
+            1
+          ]
+    },
+
+    {
+        "name":"Gunther",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,
+            5,
+            2,
+            4,
+            1,
+            1,
+            2,
+            3,
+            2,
+            1
+          ]
+    }
+]
+
