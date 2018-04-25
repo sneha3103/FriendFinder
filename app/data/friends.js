930c552bb7d632e5d84f5fcf83d86f5757ab82da
Sneha Dama <snehadama@Snehas-Air.lan>
2018-04-25 12:15:29 -0500

    Edited the friends images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
     {
         "name":"Rachel Green",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://c.tribune.com.pk/2018/02/1626217-friends-1517735643-372-640x480.jpg",
         "scores":[
             4,
             3,
@@ -18,7 +18,7 @@ var friendsArray = [
 
     {
         "name":"Chandler Bing",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://az616578.vo.msecnd.net/files/2017/02/24/6362356890381149761263534362_bing.jpg",
         "scores":[
             1,
             2,
@@ -35,7 +35,7 @@ var friendsArray = [
 
     {
         "name":"Joey Tribbiani",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"http://www.neontommy.com/sites/default/files/Screen%20Shot%202015-02-11%20at%205.04.41%20PM.png?1423704277",
         "scores":[
             5,
             1,
@@ -52,7 +52,7 @@ var friendsArray = [
 
     {
         "name":"Ross Geller",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"http://cdn.playbuzz.com/cdn/f72b59db-f12a-4ebb-bdfa-ca8a7596b797/f44812d7-2843-4547-88a1-0a04be987ad9.png",
         "scores":[
             2,
             2,
@@ -69,7 +69,7 @@ var friendsArray = [
 
     {
         "name":"Phoebe Buffet",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimages.hellogiggles.com%2Fuploads%2F2016%2F08%2F04053246%2Ffriends-1.jpg&w=700&q=85",
         "scores":[
             5,
             1,
@@ -86,7 +86,7 @@ var friendsArray = [
 
     {
         "name":"Monica Geller",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://psyc2301.wikispaces.com/file/view/friends809part1_268.jpg/134158577/432x344/friends809part1_268.jpg",
         "scores":[
             3,
             3,
@@ -104,7 +104,7 @@ var friendsArray = [
 
     {
         "name":"Janice",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://i.pinimg.com/originals/96/f5/25/96f525176a2274b3b431951b6e4e87a9.jpg",
         "scores":[
             4,
             1,
@@ -121,7 +121,7 @@ var friendsArray = [
 
     {
         "name":"Gunther",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://img.buzzfeed.com/buzzfeed-static/static/2014-09/15/22/enhanced/webdr02/original-grid-image-12319-1410834791-10.jpg?crop=385:582;57,0&downsize=715:*&output-format=auto&output-quality=auto",
         "scores":[
             1,
             5,
